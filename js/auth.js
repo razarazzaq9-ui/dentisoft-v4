84e757004ebb34332a625a9c6c42a90f2990c78e
razarazzaq9-ui <razarazzaq9@gmail.com>
2026-05-29 19:04:54 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,26 @@
-console.log("AUTH JS LOADED");
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// This must be global because HTML uses onclick="login()"
 window.login = function () {
-    alert("LOGIN CLICKED");
-}
+
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+
+    signInWithEmailAndPassword(auth, email, pass)
+        .then((userCredential) => {
+
+            alert("Login successful");
+
+            // redirect to dashboard
+            window.location.href = "dashboard.html";
+
+        })
+        .catch((error) => {
+
+            alert("Login failed: " + error.message);
+            console.log(error);
+        });
+};
