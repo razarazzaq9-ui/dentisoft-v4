1350d173805329020a66ab07f0bde75a04723497
razarazzaq9-ui <razarazzaq9@gmail.com>
2026-05-29 19:03:50 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBE48u1qeLip4OrGsx6vzvxJhYgj5vZpy0",
-    authDomain: "dentisoft-89532.firebaseapp.com",
-    projectId: "dentisoft-89532",
-    storageBucket: "dentisoft-89532.firebasestorage.app",
-    messagingSenderId: "819598110370",
-    appId: "1:819598110370:web:97b8eca32f654b015771fb",
-    measurementId: "G-JMFGRW3RYF"
-  };
+const app = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// ✅ THESE TWO EXPORTS ARE REQUIRED
+export const db = getFirestore(app);
+export const auth = getAuth(app);
