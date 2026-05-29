b76b152e340b23c220702cd69b0c2e78c5041490
razarazzaq9-ui <razarazzaq9@gmail.com>
2026-05-29 18:59:05 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,3 @@
-import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 window.login = function () {
-
-    let email = document.getElementById("email").value;
-    let pass = document.getElementById("pass").value;
-
-    signInWithEmailAndPassword(auth, email, pass)
-        .then((user) => {
-            alert("Login successful");
-            window.location.href = "dashboard.html";
-        })
-        .catch((error) => {
-            alert("Login failed: " + error.message);
-            console.log(error);
-        });
-};
+    alert("LOGIN BUTTON IS WORKING");
+}
